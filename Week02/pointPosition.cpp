1cf94db3afe83c207733b4a6251c260bb6d99f24
NShtereva <niki1shtereva@gmail.com>
2023-11-16 22:55:31 +0200

    Updated Week02

--- Week02/pointPosition.cpp
+++ Week02/pointPosition.cpp
@@ -1,97 +1,58 @@
 #include <iostream>
 #include "../vecta.h"
 
-double S(const vecta::vec2d<double>& A, const vecta::vec2d<double>& B, const vecta::vec2d<double>& C)
+typedef vecta::vec2d<double> Point;
+
+double S(const Point& A, const Point& B, const Point& C)
 {
     return (B - A) ^ (C - A); // AB x AC
 }
 
-int main()
+void pointPosition(const Point& A, const Point& B, const Point& C, const Point& P)
 {
-    vecta::vec2d<double> A, B, C, P;
-    std::cin >> A >> B >> C >> P;
-
     double SPAB = S(P, A, B),
            SPBC = S(P, B, C),
            SPCA = S(P, C, A);
 
-    if((SPAB < 0 && SPBC < 0 && SPCA < 0) || (SPAB > 0 && SPBC > 0 && SPCA > 0)) // in
+    if((SPAB < 0 && SPBC < 0 && SPCA < 0) || (SPAB > 0 && SPBC > 0 && SPCA > 0))
     {
-        std::cout << 1 << std::endl;
+        std::cout << "\nInterior point." << std::endl;
     }
     else if(SPAB == 0 && SPCA == 0) // P == A
     {
-        std::cout << 2 << std::endl;
+        std::cout << "\nP coincides with A." << std::endl;
     }
     else if(SPAB == 0 && SPBC == 0) // P == B
     {
-        std::cout << 3 << std::endl;
+        std::cout << "\nP coincides with B." << std::endl;
     }
     else if(SPBC == 0 && SPCA == 0) // P == C
     {
-        std::cout << 4 << std::endl;
+        std::cout << "\nP coincides with C." << std::endl;
     }
     else if((SPAB == 0 && SPBC < 0 && SPCA < 0) || (SPAB == 0 && SPBC > 0 && SPCA > 0)) // P e AB
     {
-        std::cout << 5 << std::endl;
+        std::cout << "\nP lies on AB." << std::endl;
     }
     else if((SPBC == 0 && SPAB < 0 && SPCA < 0) || (SPBC == 0 && SPAB > 0 && SPCA > 0)) // P e BC
     {
-        std::cout << 6 << std::endl;
+        std::cout << "\nP lies on BC." << std::endl;
     }
     else if((SPCA == 0 && SPAB < 0 && SPBC < 0) || (SPCA == 0 && SPAB > 0 && SPBC > 0)) // P e AC
     {
-        std::cout << 7 << std::endl;
+        std::cout << "\nP lies on AC." << std::endl;
     }
     else 
     {
-        std::cout << 8 << std::endl;
+        std::cout << "\nExterior point." << std::endl;
     }
-    return 0;
 }
 
-/*
-// Interior point:
-1 1
-3 1
-2 3
-1
-// P == A:
-1 1 
-3 1
-2 3
-2
-// P == B:
-1 1 
-3 1
-2 3
-3
-// P == C:
-1 1
-3 1
-2 3
-4
-// P e AB:
-1 1
-3 1
-2 3
-5
-// P e BC:
-  1 1
-  3 1
-  2 3
-2.5 2
-6
-// P e AC:
-  1 1
-  3 1
-  2 3
-1.5 2
-7
-// Exterior point:
-1 1 
-3 1
-2 3
-0 0
-8
-*/
+int main()
+{
+    Point A, B, C, P;
+    std::cin >> A >> B >> C >> P;
+
+    pointPosition(A, B, C, P);
+    return 0;
+}
